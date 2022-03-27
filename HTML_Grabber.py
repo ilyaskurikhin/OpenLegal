2d75caa52be0c18f103c0ea5c5a4bcfd90e19b81
TimoBl <63318212+TimoBl@users.noreply.github.com>
2022-03-28 00:06:30 +0200

    Great updates

--- HTML_Grabber.py
+++ HTML_Grabber.py
@@ -1,52 +1,21 @@
 import requests
 
 
-Entscheidungen = ["141 II 272",
-    "141 III 59",
-    "141 IV 266",
-    "141 IV 400",
-    "141 V 28",
-    "142 II 395",
-    "142 III 449",
-    "142 IV 110",
-    "142 V 306",
-    "143 II 215",
-    "143 II 690",
-    "143 IV 54",
-    "143 IV 125",
-    "143 V 119",
-    "144 I 251",
-    "144 II 329",
-    "144 IV 246",
-    "144 V 147",
-    "145 I 35",
-    "145 II 271",
-    "145 II 333",
-    "145 III 334",
-    "145 IV 18",
-    "145 IV 322",
-    "145 V 6",
-    "146 II 116",
-    "146 II 208",
-    "146 III 67",
-    "146 III 218",
-    "146 IV 252",
-    "146 V 63",
-    "147 I 250",
-    "147 II 28",
-    "147 III 221",
-    "147 V 58"]
+path = "entscheide.txt"
+with open(path, "r") as file:
+    Entscheidungen = file.read().splitlines()
+
 
 def API_getInfos(url):
-
     response = requests.get(url)
     return response
 
+
 for entsch in Entscheidungen:
-    entscheidungen = entsch.replace(" ", "-")
-    url="https://bger.li/"+entscheidungen
+    entscheidungen = entsch#.replace(" ", "-")
+    url = "https://bger.li/" + entscheidungen
     response = API_getInfos(url)
-    with open('output/' + entscheidungen+".html", "x", encoding='UTF-8') as file:
+    with open('output/' + entscheidungen + ".html", "w", encoding='UTF-8') as file:
         file.write(response.text)
         file.close()
 
